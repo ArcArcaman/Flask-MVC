de7877368e05b7850375c7ac0bf342950c6a8e05
ArcArcaman <hayashiiken@gmail.com>
2020-04-19 00:22:53 +0800

    Added Extensions feature for Flask-MVC. Added Default Extension: Redis client Extension <redis>

--- src/superglobal.py
+++ src/superglobal.py
@@ -1,11 +1,32 @@
 from flask import Flask
 import yaml, os
+from importlib import import_module
 
 __temp_app = Flask(__name__)
 
 __temp_app.config.from_pyfile('config.cfg')
 
 FLASK_CONFIG = __temp_app.config
+
+EXTENSIONS = dict() if FLASK_CONFIG.get('EXTENSIONS') != None else None
+for extension in FLASK_CONFIG.get('EXTENSIONS'):
+    extension_path = os.path.join(os.path.dirname(__file__), 'extensions', extension)
+    if os.path.isdir(extension_path):
+        if os.path.isfile(extension_path + '/global.yml'):
+            with open(extension_path + '/global.yml', 'r') as source:
+                EXTENSIONS[extension] = dict()
+                loaded = yaml.load(source, Loader=yaml.FullLoader)
+                __temp = import_module('extensions.' + extension)
+                for var in loaded:
+                    if type(var) == dict:
+                        EXTENSIONS[extension][list(var.keys())[0]] = var[list(var.keys())[0]]
+                    elif type(var) == str:
+                        EXTENSIONS[extension][var] = getattr(__temp, var)
+                    else:
+                        raise TypeError("Unknown variable type in global of extensions/" + extension + ". Please contact the respective developer of the extension.")
+    else:
+        raise FileNotFoundError("Extension " + extension + " not found.")
+                        
 
 USER_GLOBAL = None
 
